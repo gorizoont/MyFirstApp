098e666cdcd3e034ee13f01eaca99625b71f59ca
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-09-04 14:00:24 +0300

    mig

--- MyFirstApp/Data/Migrations/20220904105600_mssql.local_migration_531.Designer.cs
+++ MyFirstApp/Data/Migrations/20220904105600_mssql.local_migration_531.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyFirstApp.Data;
 
 namespace MyFirstApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220904105600_mssql.local_migration_531")]
+    partial class mssqllocal_migration_531
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
